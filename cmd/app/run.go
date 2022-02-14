1132dad2d57bb2edbbaff8d9c17cdfae226fe87f
yjunya <38554985+yjunya@users.noreply.github.com>
2022-02-14 18:36:18 +0900

    環境変数の追加 (#1)
    * init
    
    * feat: add config

--- cmd/app/run.go
+++ cmd/app/run.go
@@ -4,9 +4,13 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/yjunya/api-example/project/config"
 )
 
 func Run() {
+	config.InitConfig()
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/friendsofgo/errors v0.9.2
 	github.com/gin-gonic/gin v1.7.7
+	github.com/joho/godotenv v1.4.0
 	github.com/volatiletech/sqlboiler/v4 v4.8.6
 	github.com/volatiletech/strmangle v0.0.1
 )
